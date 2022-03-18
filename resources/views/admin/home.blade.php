22e455e21e479377fe407eaebd5442857434f6c3
antoniom28 <antomarc@gmail.com>
2022-03-18 14:45:51 +0100

    creato nuovo controller,cambiare avatar dopo login

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -17,7 +17,23 @@
                         @if(Auth::user()->avatar)
                             <img src="{{ asset("storage/".Auth::user()->avatar) }}" alt="">
                         @else
-                            <h1 class="text-center m-5">Carica un'avatar ! (è solo testo..)</h1>
+                                <form action="{{route("admin.users.update" , Auth::user())}}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    @method('PUT')
+                                <div class="form-group">
+                                    <label for="avatar">Image</label>
+                                    <input type="file" name="avatar" class="p-1 form-control @error('avatar') is-invalid @enderror ">
+                                </div>
+                                @error('avatar')
+                                <div class="invalid-feedback">
+                                    {{$message}}
+                                </div>
+                                @enderror
+
+                                <button type="submit" class="btn btn-primary">
+                                    Vai
+                                </button>
+                            </form>
                         @endif
                     </div>
                     <div class="text-center">
